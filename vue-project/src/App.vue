c932027c137eed8cb92e0f371ff6e3f966563c97
alexiscenriquez <alexiscenriquez@hotmail.com>
2022-04-09 20:51:39 -0500

    fixed navbar

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,67 +1,102 @@
 <template>
-  <nav class="navbar navbar-expand navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand float-left" href="/" id="brand">
-        Bread of Life / CFC
-      </a>
-      <ul class="nav navbar-nav mr-auto" id="navbar">
-        <li class="nav-item">
-          <router-link class="nav-link" to="/">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/create">Clients</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/view">Volunteers</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/view">Employees</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/about">About</router-link>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid" id="navbar">
+      <a class="navbar-brand logo" href="#">CFC/BOL</a>
+      <div>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Create
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Clients</a></li>
+                <li><a class="dropdown-item" href="#">Volunteers</a></li>
+                <li><a class="dropdown-item" href="#">Events</a></li>
+                <li></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                View
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Clients</a></li>
+                <li><a class="dropdown-item" href="#">Volunteers</a></li>
+                <li><a class="dropdown-item" href="#">Events</a></li>
+                <li><a class="dropdown-item" href="#">Employees</a></li>
+                <li><a class="dropdown-item" href="#">Services</a></li>
+                <li></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link">About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </nav>
 </template>
 
 <style scoped>
 @import "./assets/base.css";
-
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto&display=swap");
+body {
+}
 #app {
   max-width: 1280px;
   margin: 0 auto;
 
   font-weight: normal;
 }
-
 nav {
-  margin-top: 0;
-  width: 100vw;
+  font-size: 1.1em;
+  padding: 20px;
+  font-family: "Roboto", sans-serif !important;
 }
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+a:hover {
+  font-size: 20px;
+  padding-bottom: 0px;
 }
 .container {
-  padding: 5px;
+  padding: 15px;
 }
-li:hover {
-  cursor: pointer;
-}
+
 li {
   margin-right: 10px;
-}
-#brand:hover {
-  color: orange;
-}
-#navbar.nav-link:hover {
-  color: orange;
 }
 
 @media (hover: hover) {
 }
-
 
 @media (min-width: 1024px) {
   body {
