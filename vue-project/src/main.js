8a7a8280060e9e90cceb7f1262a16ad11a09c813
gaguirr5 <77712470+gaguirr5@users.noreply.github.com>
2022-04-10 20:50:10 -0500

    Delete vue-project directory
    files have been moved

--- vue-project/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-//import router from "../router/index.js"
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
-createApp(App).mount("#app");
