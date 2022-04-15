da3aa79f4bb971e144f9d41b072f16fa72539aae
Taylor Bean <tjbean@uh.edu>
2022-04-14 23:24:07 -0500

    Fix Backend, add new route to BE and FE

--- Frontend/src/views/clients/ClientsEdit.vue
+++ Frontend/src/views/clients/ClientsEdit.vue
@@ -2,8 +2,8 @@
     <div class="row justify-content-center">
         <div class="col-md-6">
             <!-- Update Student content -->
-            <h3 class="text-center">Update Event</h3>
-            <form @submit.prevent="UpdateEvent">
+            <h3 class="text-center">Update Client</h3>
+            <form @submit.prevent="UpdateClient">
 
                 <div class="form-group">
                     <label>ID {{cls_list[0].cid}}</label>
@@ -278,7 +278,7 @@ export default {
         });
     },
     methods: {
-        UpdateEvent() {
+        UpdateClient() {
             let apiURL = `http://localhost:8080/clients/update/${this.$route.params.id}`;
 
             axios.put(apiURL, this.cls_list[0]).then((res) => {
