d5c340ad436c3f18d688f6ad4d915a92fb4fbc58
gaguirr5 <77712470+gaguirr5@users.noreply.github.com>
2022-04-18 12:36:22 -0500

    report start

---
+++ Backend/routes/reports.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const eventsModel = require('../models/events')
+const volunteerModel = require('../models/volunteers')
+const clientsModel = require('../models/clients')
+router.get('/ethnicity', (req, res, next) =>{
+    
+})
+
+module.exports = router;
